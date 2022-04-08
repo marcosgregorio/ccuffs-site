343ead250368d24a33849edc81e3491494aee2fa
marcos <marquinhogreg2013@gmail.com>
2022-04-08 14:55:16 -0300

    site[

--- script/script.js
+++ script/script.js
@@ -1,1 +1,18 @@
-console.log():
+const imagens = document.querySelectorAll('#galeria li img');
+
+function galeriaTrocar(event){
+    
+    const principal = document.querySelector("#img-principal")
+    const clicada = event.currentTarget;
+    principal.src = clicada.src;
+    principal.alt = clicada.alt;            
+
+}
+
+function galeriaClique(imagem){
+    imagem.addEventListener('click', galeriaTrocar);
+}
+
+imagens.forEach(galeriaClique);
+
+//CÓDIGO PARA FAZER A GALERIA//
